1179eddae90f09c63c83d15c6c86b5c16922c06a
Łukasz Piotrowski <lukasz.piotrowski@whiteaster.com>
2020-04-09 00:30:53 +0200

    fix manual issiues

--- ns/src/app/components/manual/manual.component.ts
+++ ns/src/app/components/manual/manual.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, OnDestroy } from "@angular/core";
 import { TouchGestureEventData } from "tns-core-modules/ui/gestures/gestures";
 import { HttpClient } from "@angular/common/http";
 import { RouterExtensions } from "nativescript-angular/router";
 import { StoreService } from "~/app/services/store.service";
+import { timer, Observable, Subscription } from "rxjs";
 
 @Component({
     selector: "ns-manual",
@@ -10,22 +11,28 @@ import { StoreService } from "~/app/services/store.service";
     styleUrls: ["./manual.component.css"],
     moduleId: module.id,
 })
-export class ManualComponent implements OnInit {
-    public event: any;
+export class ManualComponent implements OnInit, OnDestroy {
     private touched: boolean;
+    private finish: boolean;
     public speed: any;
     public position: { x: string; y: string; z: string };
-    private zero: number;
+    private count: number;
+    private current: number;
+    public sub: Subscription = new Subscription();
+    public timer: Observable<any>;
+    public startSub = true;
+    public axis = "";
 
     constructor(
         private httpClient: HttpClient,
         private router: RouterExtensions,
         private store: StoreService
     ) {
-        this.event = [0, 0];
         this.touched = false;
-        this.speed = 1;
-        this.zero = 0;
+        this.finish = false;
+        this.speed = 16;
+        this.count = 0;
+        this.current = 0;
         this.position = {
             x: "0",
             y: "0",
@@ -33,21 +40,14 @@ export class ManualComponent implements OnInit {
         };
     }
 
-    private action(axis: string, direction: number): void {
-        // console.log(
-        //     `ns-in----manual--${axis}---- ` +
-        //         JSON.stringify({
-        //             action: "start",
-        //             time: this.store.getNowDate(),
-        //         })
-        // );
+    private action(direction: number, action: boolean): void {
         this.httpClient
             .post(
                 "http://192.168.43.77:3000/api/motor",
                 {
-                    axis,
+                    axis: this.axis,
                     direction,
-                    action: this.touched,
+                    action,
                     speed: this.speed,
                 },
                 {
@@ -55,62 +55,64 @@ export class ManualComponent implements OnInit {
                 }
             )
             .subscribe((res: any) => {
-                // console.log("pi-out---manual-res--- " + JSON.stringify(res));
                 this.position = res;
+                if (this.finish) {
+                    console.log(
+                        "pi-out---manual-res--- " + JSON.stringify(res)
+                    );
+                }
             });
     }
 
-    public onTouchHelper(
-        enter: () => void,
-        leave: () => void,
-        event: TouchGestureEventData
-    ): void {
-        if (!this.touched) {
-            // console.log(1);
-            enter();
-        }
-        if (this.event[0] == event.getX() && this.event[1] == event.getY()) {
-            // console.log(0);
-            // console.log(this.event);
-            // console.log(event.getX());
-            // console.log(event.getY());
-            leave();
-        }
-        this.event = [event.getX(), event.getY()];
-    }
+    public onTouch(axis: string, direction: number): void {
+        this.axis = axis;
+        this.count++;
 
-    public onTouch(
-        axis: string,
-        direction: number,
-        event: TouchGestureEventData
-    ): void {
-        this.onTouchHelper(
-            () => {
-                // console.log(1);
-                this.touched = true;
-                this.action(axis, direction);
-            },
-            () => {
-                // console.log(0);
-                // this.touched = false;
-                // this.action(axis, direction);
-            },
-            event
-        );
-        // this.touched = false;
-        // console.log("touch");
+        if (!this.touched) {
+            this.touched = true;
+            this.finish = false;
+            this.action(direction, true);
+            console.log(
+                `ns-in----manual--${axis}---- ` +
+                    JSON.stringify({
+                        action: "start",
+                        time: this.store.getNowDate(),
+                    })
+            );
+        } else {
+            if (this.startSub) {
+                this.startSub = false;
+                this.timer = timer(100, 100);
+                this.sub.add(
+                    this.timer.subscribe((data) => {
+                        if (
+                            this.touched &&
+                            !this.finish &&
+                            this.count === this.current
+                        ) {
+                            this.finish = true;
+                            this.current = 0;
+                            this.count = 0;
+                            this.touched = false;
+                            this.action(direction, false);
+                            console.log(
+                                `ns-in----manual--${axis}---- ` +
+                                    JSON.stringify({
+                                        action: "stop",
+                                        time: this.store.getNowDate(),
+                                    })
+                            );
+                        } else {
+                            this.current = this.count;
+                        }
+                    })
+                );
+            }
+        }
     }
 
     public changeSpeed(speed: number, event: TouchGestureEventData): void {
-        this.onTouchHelper(
-            () => {
-                this.speed = speed;
-            },
-            () => {
-                console.log(this.speed);
-            },
-            event
-        );
+        this.speed = speed;
     }
 
     public goToAuto(): void {
@@ -118,4 +120,8 @@ export class ManualComponent implements OnInit {
     }
 
     ngOnInit() {}
+
+    ngOnDestroy() {
+        this.sub.unsubscribe();
+    }
 }
--- raspberry/app/dist/http/http.js
+++ raspberry/app/dist/http/http.js
@@ -3,11 +3,11 @@ Object.defineProperty(exports, "__esModule", { value: true });
 class Http {
     stepperStrategy(req, start, stop) {
         if (req.body.action === true) {
-            console.log(1);
+            console.log(1, "---");
             start();
         }
         if (req.body.action === false) {
-            console.log(0);
+            console.log(0, "---");
             stop();
         }
     }
